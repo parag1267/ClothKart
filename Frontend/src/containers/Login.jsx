6c641275fd99645d9f7be473dadeb130bcd9f8b1
parag1267 <hadiyaparag40@gmail.com>
2026-05-09 16:39:14 +0530

    Complate Solution

--- Frontend/src/containers/Login.jsx
+++ Frontend/src/containers/Login.jsx
@@ -48,7 +48,7 @@ const Login = () => {
   });
 
   const handleGoogleLogin = () => {
-    window.location.href = `${import.meta.env.BASE_URL}/api/auth/google`;
+    window.location.href = `${import.meta.env.VITE_API_BASE_URL}/api/auth/google`;
   }
 
   return (
