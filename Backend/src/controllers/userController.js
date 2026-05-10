67180f355f8b33a43ba5c42a5ec58849c0a1ec2a
parag1267 <hadiyaparag40@gmail.com>
2026-05-10 14:22:20 +0530

    All Done

--- Backend/src/controllers/userController.js
+++ Backend/src/controllers/userController.js
@@ -173,11 +173,13 @@ const signin = async (req, res) => {
         res
             .cookie("accessToken", accessToken, {
                 httpOnly: true,
-                secure: false
+                secure: true,
+                sameSite: "None"
             })
             .cookie("refreshToken", refreshToken, {
                 httpOnly: true,
-                secure: false
+                secure: true,
+                sameSite: "None"
             })
             .status(200).json({
                 success: true,
@@ -207,8 +209,8 @@ const logout = async (req, res) => {
 
         const cookieOptions = {
             httpOnly: true,
-            secure: false,
-            sameSite: 'lax'
+            secure: true,
+            sameSite: 'None'
         }
 
         res
@@ -375,11 +377,13 @@ const googleAuthCallback = async (req, res) => {
         res
             .cookie("accessToken", accessToken, {
                 httpOnly: true,
-                secure: false
+                secure: true,
+                sameSite: "None"
             })
             .cookie("refreshToken", refreshToken, {
                 httpOnly: true,
-                secure: false
+                secure: true,
+                sameSite: "None"
             })
             .redirect(`${process.env.FRONTEND_URL}/`);
     } catch (error) {
