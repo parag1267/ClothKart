fdbacd8188ff0178f72c8784362e0d3c5d91e535
parag1267 <hadiyaparag40@gmail.com>
2026-05-10 15:05:41 +0530

    Fixed bug login error

--- Frontend/src/features/auth/authSlice.js
+++ Frontend/src/features/auth/authSlice.js
@@ -17,7 +17,7 @@ export const registerUser = createAsyncThunk(
     async (userData, { rejectWithValue }) => {
         try {
             const res = await axios.post(
-                "https://clothing-e-commerce-web-application.onrender.com/api/auth/signup",
+                "/api/auth/signup",
                 userData,
                 { withCredentials: true }
             );
@@ -35,7 +35,7 @@ export const loginUser = createAsyncThunk(
     async (userData, { rejectWithValue }) => {
         try {
             const res = await axios.post(
-                "https://clothing-e-commerce-web-application.onrender.com/api/auth/signin",
+                "/api/auth/signin",
                 userData,
                 { withCredentials: true }
             );
@@ -57,7 +57,7 @@ export const fetchUserProfile = createAsyncThunk(
     async (_, { rejectWithValue }) => {
         try {
             const res = await axios.get(
-                "https://clothing-e-commerce-web-application.onrender.com/api/auth/profile",
+                "/api/auth/profile",
                 { withCredentials: true }
             );
             return res.data.user;
@@ -75,7 +75,7 @@ export const logoutUser = createAsyncThunk(
     async (_, { dispatch, rejectWithValue }) => {
         try {
             await axios.post(
-                "https://clothing-e-commerce-web-application.onrender.com/api/auth/logout",
+                "/api/auth/logout",
                 {},
                 { withCredentials: true }
             )
@@ -92,7 +92,7 @@ export const googleUser = createAsyncThunk(
     async (_, { rejectWithValue }) => {
         try {
             const res = await axios.get(
-                "https://clothing-e-commerce-web-application.onrender.com/api/auth/profile",
+                "/api/auth/profile",
                 { withCredentials: true }
             )
 
